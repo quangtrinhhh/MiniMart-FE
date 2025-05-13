4242d8d815d15543751cfa0e3bdd102d5b1323a3
quangtrinhhh <trinhngocquanggg@gmail.com>
2025-05-13 22:12:31 +0700

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -25,6 +26,13 @@ const nextConfig: NextConfig = {
       },
     ],
     domains: ["fonts.googleapis.com"],
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "src"), // Thêm alias @ trỏ tới thư mục src
+    };
+    return config;
   },
 };
 
